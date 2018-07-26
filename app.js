395aae279ec5929a113704c518096b9314d6682f
framirez <framirez@folderit.net>
2018-07-26 17:33:54 -0300

    conección a la BD (no funciona hasta este punto)

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//app.use('/api/v1/users', users);
+app.use('/api/v1/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,7 +46,7 @@ app.use(function(req, res, next){
     res.locals.connection = mysql.createConnection({
         host     : 'localhost',
         user     : 'root',
-        password : ' ',
+        //password : ' ',
         database : 'test'
     });
     res.locals.connect();
